17ecb50561136923d5d0afd6192d871d84ccef7e
kenesbaevnagozzal <xudaybergenovagozzal670@gmail.com>
2024-07-05 19:55:17 +0800

    Add files via upload

---
+++ begin.py
@@ -0,0 +1,407 @@
+# Begin
+import math
+# 1 
+# a = float(input("a = "))
+# P = 4 * a
+# print("P = ", P)
+
+
+# 2
+# a = float(input("a = "))
+# import math 
+# S = a ** a
+# print("S = ", S)
+
+
+# 3
+# a = float(input("a = "))
+# b = float(input("b = "))
+# P = 2 * (a + b)
+# S = a * b
+# print("S = ", S)
+# print("P = ", P)
+
+
+
+# 4 
+# d = float(input("d = "))
+# L =3.14 * d
+# print("L = ", L)
+
+
+# 5 
+# a = float(input("a = "))
+# V = a * a * a
+# S = 6 * (a * a)
+# print("V = ",  V)
+# print("S = ", S)
+
+
+
+
+# 6
+# a = float(input("a = "))
+# b =float(input("b = "))
+# c =float(input("c ="))
+# V = a * b * c
+# S = 2 * ( a * b * c)
+# print("V = ", V)
+# print("S = ", S)
+
+
+
+# 7
+# R = float(input(" R ="))
+# L = 2 * 3.14 * R
+# S = 3.14 * R * R
+# print("L = ", L)
+# print("S = ", S)
+
+
+
+
+# 8
+# a = float(input("a = "))
+# b =float(input("b = "))
+# c = (a + b ) / 2
+# print("c = ",  c)
+
+
+
+
+# 9
+# a = float(input("a = "))
+# b =float(input("b = "))
+# m = math.sqrt (a * b)
+# print( " m = ", m)
+
+
+
+# 10 
+# a = float(input("a = "))
+# b =float(input("b = "))
+# a != 0
+# b != 0
+# c = ((a + b ),   (a * b ),   (a ** a),   ( b ** b  ))
+# print("c = ", c )
+
+# 11   
+
+
+# A = float(input("A = "))
+# B = float(input("B = "))
+# A != 0
+# B != 0
+# print("A + B ",  A + B)
+# print("A * B",   A * B)
+# print((abs(A)),   (abs(B)))
+
+
+
+# # 12
+# a = float(input("a = "))
+# b =float(input("b = "))
+# c = ((a ** a) + (b ** b ))
+# P = a + b + c
+# print("c = ", c)
+# print("P = ", P )
+
+
+
+
+# # 13 
+# R1 = float(input("R1 = "))
+# R2 = float(input("R2 = "))
+# S1 = 3.14 * R1
+# S2 = 3.14 * R2
+# S3 = 3.14 * (R1 - R2 )
+# print("S1 = ", S1 )
+# print( "S2 = ", S2)
+# print("S3 = ", S3)
+
+
+
+
+
+# 14
+# L = float(input("L = "))
+# R = L / 2 * 3.14
+# S = 3.14 * ( R * R )
+# print("R = ", R)
+# print("S = ", S)
+
+
+# # 15 
+
+# s = float(input('s = '))
+# r = math.sqrt(s)/3.14
+
+# print('r = ', r)
+
+
+# # 16 
+# x1 = float(input("x1 ="))
+# x2 = float(input("x2 = "))
+# m = math.abs(x1 - x2)
+# print("m = ", m)
+
+# # # 17
+# a = float(input("a = "))
+# b = float(input("b = "))
+# c = float(input("c = "))
+# n = abs(a - c)
+# m = abs(b - c)                        
+# z = n + m
+# print("z = ", z)
+
+
+# # 18 
+# x1 = float(input("x1 = "))
+# x2 = float(input("x2 = "))
+# y1 = float(input("y1 = "))
+# y2 = float(input("y2 = "))
+# a = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+# print(a)
+
+# 19 
+
+
+# x1 = float(input("x1 = "))
+# x2 = float(input("x2 = "))
+# y1 = float(input("y1 = "))
+# y2 = float(input("y2 = "))
+# x3 = float(input("x3 = "))
+# x4 = float(input("x4 = "))
+# y3 = float(input("y3 = "))
+# y4 = float(input("y4 = "))
+# x1 == x3
+# x2 == x4
+# y1 == y2
+# y3 == y4
+# a = math.sqrt((x4-x3)**2 + (y4-y3)**2)
+# b = math.sqrt((x4-x2)**2 + (y4-y2)**2)
+# S = a * b
+# print("S = ", S)
+    
+    # 20
+
+# x1 = float(input("x1 = "))
+# x2 = float(input("x2 = "))
+# y1 = float(input("y1 = "))
+# y2 = float(input("y2 = "))
+# a = math.sqrt((x2-x1)**2 + (y2-y1)**2)
+# print(a)
+
+# 21 
+
+
+# x1 = float(input("x1 = "))
+# x2 = float(input("x2 = "))
+# x3 = float(input("x3 = "))
+# y1 = float(input("y1 = "))
+# y2 = float(input("y2 = "))
+# y3 = float(input("Y3 = "))
+# a = math.sqrt((x2-x1)**2 + (y2-y1)**2)
+# b = math.sqrt((x3-x1)**2 + (y3-y1)**2)
+# c = math.sqrt((x3-x2)**2 + (y3-y2)**2)
+# p = (a + b + c) / 2
+# s = math.sqrt(p*(p-a)*(p-b)*(p-c))
+# print("p = ", p) 
+# print("s = ", s)
+
+# # 22 
+# a = int(input("a = "))
+# b = int(input("b = "))
+# c = a
+# a = b
+# b = c
+# print(a , b)
+
+# # 23 
+# a = int(input("a = "))
+# b = int(input("b = "))
+# c = int(input("c = "))
+# d = a
+# a = b 
+# b = c
+# c = d
+# print(a , b ,c )
+
+
+
+# 24
+
+# a = float(input("a = "))
+# b = float(input("b = "))
+# c = float(input("c = "))
+# x = float(b)
+# y = float(c)
+# z = float(a)
+# print(x)
+# print(y)
+# print(z)
+
+# # 25
+# x = float(input("x = "))
+# y = 3*x**6 - 6*x**2 - 7
+# print(y)
+
+# # 26
+# x = float(input("x ="))
+# y = 4*(x-3)**6 - 7*(x-3) + 2
+# print(y)
+
+# #27
+# A = int(input("A = "))
+# print(math.pow(A,2))            
+# print(math.pow(A,4))
+# print(math.pow(A,8))
+
+# # 28
+# A = int(input("A ="))
+# print(pow(A,2))
+# print(math.pow(A,3))
+# print(math.pow(A,5))
+# print(math.pow(A,10))
+# print(math.pow(A,15))
+
+# # 29
+# alpha = float(input("alpha = "))
+# rad = alpha * 3.14/180
+# print("Rad = ", rad)
+
+# 30
+# alpha = float(input("alpha = "))
+# rad = alpha * 3.14/180
+# print("Rad = ", rad)
+
+# # 31
+# Tf = float(input("Tf = "))
+# Tc = (Tf -32)*5/9
+# print("Tc = ", Tc)
+
+# #32 
+# Tc = float(input("Tc = "))
+# Tf = Tc*9/5 + 32
+# print("Tf = ", Tf)
+
+# # 33
+
+
+# A = float(input("A = "))
+# x = float(input("x = "))
+# y = float(input("y = "))
+# s= A/x
+# S = A/x*y
+# print("s = ",  s)
+# print("S = ", S)
+
+
+
+# 34
+
+
+# A = float(input("A = "))
+# x = float(input("x = "))
+# y = float(input("y = "))
+# B = float(input("B = "))
+# z = A/x
+# c = B/y
+# print(abs(c - z))
+
+
+
+# #35 
+# v = float(input("v = "))  # qayiqtin tezligi 
+# u = float(input("u = "))    # agistin tezligi 
+# t1 = float(input("t1 ="))    # agis boynsha 
+# t2 = float(input("t2 = "))    # agisqa qarsi
+
+
+
+
+# # 36 
+# v1 = int(input("v1 = "))
+# v2 = int(input("v2 = "))
+# t = int(input("t = "))
+# s = int(input("s = "))
+# s1 = v1 *t
+# s2 = v2 *t
+# s3 = s - (s1 - s2)
+# print("s3 = ",  s3)
+
+
+# 37
+
+# v1 = int(input("v1 = "))
+# v2 = int(input("v2 = "))
+# t = int(input("t = "))
+# s = int(input("s = "))
+# s1 = v1 *t
+# s2 = v2 *t
+# s3 = s - (s1 + s2)
+# print("s3 = ",  s3)
+
+
+
+# 38
+
+# A = float(input("A = "))
+# B = float(input("B = "))
+# x = - B/A
+# print("x = ",  x)
+
+
+
+# 39
+ 
+
+# A = float(input("A = "))
+# B = float(input("B = "))
+# C = float(input("C = "))
+# D = math.sqrt(B*B - 4*A*C)
+# x1 = (-B + D)/2*A
+# x2 = (-B - D)/2*A
+# print("x1 = ",  x1)
+# print("x2 = ", x2)
+
+
+# 40
+ 
+
+
+# A1 = float(input("A = "))
+# B1 = float(input("B = "))
+# C1 = float(input("C = "))
+# A2 = float(input("A = "))
+# B2 = float(input("B = "))
+# C2= float(input("C = "))
+# D = (A1*B2 - A2*B1)
+# y = (A1*C2 - A2*C1 )/D
+# x = (B1*C2 - B2*C1)/D
+# print(x)
+# print(y)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
